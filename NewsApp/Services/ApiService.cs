093a915b907362b7afff2921c37fccae2b2b8adc
LeafyIsHere <89535173+KatelynTheGreat@users.noreply.github.com>
2023-01-05 09:03:40 -0500

    Add project files.

---
+++ NewsApp/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=6c762e15e041f6522f9add6d1f045707&topic=breaking-news&lang=en&topic=sports");
+
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
